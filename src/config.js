0b5c03f7f886daf3adfc09ea7cbfd6d6a8364b7e
Uhmopt <qethum@gmail.com>
2022-07-17 16:20:12 -0700

    [New] code

--- src/config.js
+++ src/config.js
@@ -5,6 +5,8 @@ export const APP_NAME = "dynamic-multimedia-challenge";
 
 export const REPOSITORY =
   "https://github.com/Uhmopt/dynamic-multimedia-challenge-ui";
+export const REPOSITORY_API =
+  "https://github.com/Uhmopt/dynamic-multimedia-challenge-api";
 
 export const GITHUB = "https://github.com/Uhmopt";
 export const GITHUB_NAME = "Uhmopt";
--- src/views/layout/header/menuData.js
+++ src/views/layout/header/menuData.js
@@ -1,9 +1,11 @@
-import { Code, GitHub } from "@mui/icons-material";
+import { Code, GitHub, Html } from "@mui/icons-material";
+import { REPOSITORY_API } from "config";
 import { REPOSITORY } from "config";
 import { GITHUB } from "config";
 import React from "react";
 
 export const MENU_DATA = [
   { label: "GITHUB", to: GITHUB, icon: <GitHub /> },
-  { label: "Code", to: REPOSITORY, icon: <Code /> },
+  { label: "Frontend", to: REPOSITORY, icon: <Html /> },
+  { label: "Backend", to: REPOSITORY_API, icon: <Code /> },
 ];
